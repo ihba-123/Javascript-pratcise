ebe68fe628c8b89473c453702c0e7b0529461827
Abhishek Bhatta <abhishekbhatta110@gmail.com>
2024-08-12 21:17:35 +0545

    Add explaination

--- Asynchronous function/script.js
+++ Asynchronous function/script.js
@@ -44,7 +44,21 @@ function startChanging(){
 
 function stopChanging(){
     clearInterval(setint);
+    setint = null;
 }
 
 document.querySelector("#start").addEventListener("click",startChanging);
 document.querySelector("#stop").addEventListener("click",stopChanging);
+
+
+
+// XMLHttpRequest (XHR) objects are used to interact with servers. You can retrieve data from a URL without having to do a full page refresh. This enables a Web page to update just part of a page without disrupting what the user is doing.
+ 
+/*
+Value	State	                             Description
+0	    UNSENT	                             Client has been created. open() not called yet.
+1	    OPENED	                             open() has been called.
+2	    HEADERS_RECEIVED	                 send() has been called, and headers and status are available.
+3	    LOADING	                             Downloading; responseText holds partial data.
+4	    DONE	                             The operation is complete.
+*/
