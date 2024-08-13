e64e8a8314f83c62031c7fdd4a6f32f8d892117d
Abhishek Bhatta <abhishekbhatta110@gmail.com>
2024-08-13 21:23:07 +0545

    Add ApiRequest

---
+++ ApiRequest and v8 engines/api.js
@@ -0,0 +1,6 @@
+const picture = document.getElementById("pic");
+const btn = document.getElementById("btns");
+
+btn.addEventListener("click", () => {
+    picture.src = 'https://th.bing.com/th/id/OIP.A07WhNDHO3ffqfJfMmwFeAHaEK?w=318&h=180&c=7&r=0&o=5&pid=1.7';
+})
